3b710dbf4a4d3102ef6680b28011b5e1a51e232c
PrabeshTimilsina <prabesht09@gmail.com>
2024-12-22 19:24:58 +0545

    File structure and initial app

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="COVID-19 Dashboard",
+    page_icon="🌍",
+    layout="wide",
+)
+
+st.title("🌍 COVID-19 Data Visualization Dashboard")
+st.markdown(
+    """
+    Welcome to the COVID-19 Dashboard!  
+    Use the sidebar to navigate between:
+    - **🏠 Home**
+    - **📈 Trends**
+    - **💉 Vaccinations**
+    - **🌍 Map**
+    """
+)
+st.sidebar.success("Select a page above to get started.")
